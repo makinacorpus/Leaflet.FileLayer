21f3ab6d8a2bb5e00cd08163862468806fb574de
Marco <tentimarco0@gmail.com>
2015-11-15 20:57:43 +0100

    Add csv parser.

--- leaflet.filelayer.js
+++ leaflet.filelayer.js
@@ -9,7 +9,11 @@ var FileLoader = L.Class.extend({
     includes: L.Mixin.Events,
     options: {
         layerOptions: {},
-        fileSizeLimit: 1024
+        fileSizeLimit: 1024,
+        firstLineTitles: true,
+        latitudeTitle: 'lat',
+        longitudeTitle: 'lng',
+        titleForSearch: 'title'
     },
 
     initialize: function (map, options) {
@@ -17,9 +21,11 @@ var FileLoader = L.Class.extend({
         L.Util.setOptions(this, options);
 
         this._parsers = {
+            'json': this._loadGeoJSON,
             'geojson': this._loadGeoJSON,
             'gpx': this._convertToGeoJSON,
-            'kml': this._convertToGeoJSON
+            'kml': this._convertToGeoJSON,
+            'csv': this._convertCsvToGeoJSON
         };
     },
 
@@ -82,13 +88,81 @@ var FileLoader = L.Class.extend({
         }
         var geojson = toGeoJSON[format](content);
         return this._loadGeoJSON(geojson);
-    }
+    },
+
+    /*
+     *  Added from 4535992.
+     */
+    _convertCsvToGeoJSON: function(content){
+        try {
+            if(!this.options.firstLineTitles){
+                throw new Error('The file CSV must have the Headers');
+            }
+            var geojson;
+            /*
+             * for this function i used the Papa Parser of mholt
+             * (https://github.com/mholt/PapaParse)
+             */
+            geojson = Papa.parse(content,{header: this.options.firstLineTitles}); //convert csv to json.
+            geojson = this._addFeatureToJson(geojson);
+            return this._loadGeoJSON(geojson);
+        }catch(e){alert(e.message);}
+    },
+
+    _addFeatureToJson: function(papaJson){
+        papaJson["type"]="FeatureCollection";
+        papaJson["features"]=[];
+        var title = papaJson.meta.fields;
+        for (var num_linea = 0; num_linea < papaJson.data.length - 1; num_linea++) { //  var depth = papaJson.data.length - 1;
+            var obj = papaJson.data[num_linea]; //single element of papa parse json object
+            var fields = title.toString().trim().split(",");
+            var lng = parseFloat(obj[this.options.longitudeTitle]);
+            var lat = parseFloat(obj[this.options.latitudeTitle]);
+            if (fields.length+"=="+title.length && lng<180 && lng>-180 && lat<90 && lat>-90) {
+                var feature = {};
+                feature["type"]="Feature";
+                feature["geometry"]={};
+                feature["properties"]={};
+                feature["geometry"]["type"]="Point";
+                feature["geometry"]["coordinates"]=[lng,lat];
+                var content ='<div class="popup-content"><table class="table table-striped table-bordered table-condensed">';
+                for (var i=0; i< title.length; i++) {
+                    if (title[i] != this.options.latitudeTitle && title[i] != this.options.longitudeTitle) {
+                        if(title[i]==this.options.titleForSearch){
+                            feature["properties"]["search"]=this._deleteDoubleQuotes(obj[title[i]]);
+                            feature["properties"]["title"]=this._deleteDoubleQuotes(obj[title[i]]);
+                        }else{
+                            feature["properties"][title[i]] = this._deleteDoubleQuotes(obj[title[i]]);
+                            var href='';
+                            if (obj[title[i]].indexOf('http') === 0) {
+                                href = '<a target="_blank" href="' + obj[title[i]] + '">'+ obj[title[i]] + '</a>';
+                            }
+                            if(href.length > 0)content += '<tr><th>'+title[i]+'</th><td>'+ href +'</td></tr>';
+                            else content += '<tr><th>'+title[i]+'</th><td>'+ obj[title[i]] +'</td></tr>';
+                        }
+                    }//end of if
+                }//end of for
+                content += "</table></div>";
+                feature["properties"]["popupContent"]=content;
+                papaJson["features"].push(feature);
+            }
+        }
+        return papaJson;
+    },
+
+    _deleteDoubleQuotes: function (text) {
+        text = text.trim().replace(/^"/,"").replace(/"$/,"");
+        return text;
+    },
+
+    _propertiesNames: []
+
 });
 
 
 L.Control.FileLayerLoad = L.Control.extend({
     statics: {
-        TITLE: 'Load local file (GPX, KML, GeoJSON)',
+        TITLE: 'Load local file (GPX, KML, GeoJSON, CSV)',
         LABEL: '&#8965;'
     },
     options: {
@@ -172,7 +246,7 @@ L.Control.FileLayerLoad = L.Control.extend({
         var fileInput = L.DomUtil.create('input', 'hidden', container);
         fileInput.type = 'file';
         if (!this.options.formats) {
-            fileInput.accept = '.gpx,.kml,.geojson';
+            fileInput.accept = '.gpx,.kml,.json,,.geojson,.csv';
         } else {
             fileInput.accept = this.options.formats.join(',');
         }
