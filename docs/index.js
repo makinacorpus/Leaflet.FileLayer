d31bcc9e9e50d837e7e1bfc9696c2e3aa9fa4bef
SBats <simon.bats12@gmail.com>
2017-11-17 23:12:31 -0500

    Add layers use exemple

--- docs/index.js
+++ docs/index.js
@@ -18,18 +18,23 @@
             clickable: false
         };
         L.Control.FileLayerLoad.LABEL = '<img class="icon" src="folder.svg" alt="file icon"/>';
-        L.Control.fileLayerLoad({
+        control = L.Control.fileLayerLoad({
             fitBounds: true,
             layerOptions: {
                 style: style,
                 pointToLayer: function (data, latlng) {
                     return L.circleMarker(
-                    latlng,
-                    { style: style }
+                        latlng,
+                        { style: style }
                     );
                 }
             }
-        }).addTo(map);
+        });
+        control.addTo(map);
+        control.loader.on('data:loaded', function (e) {
+            var layer = e.layer;
+            console.log(layer);
+        });
     }
 
     window.addEventListener('load', function () {
